a34ded2bb5ed943a606f34a056209f55b2b8dbe8
Michał Podgórny <m.podgorny@bonasoft.pl>
2021-09-13 00:46:13 +0200

    cors fix

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const { default: axios } = require("axios");
+var port = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
-var port = process.env.PORT || 8080;
+var cors = require("cors");
+
+app.use(cors());
+app.set('trust proxy', true);
 
 app.get("/*", (req, res) => {
   let parsedParam = "";
   if (typeof req?.params?.["0"] === "string") {
     parsedParam = req.params["0"];
   }
+  if (parsedParam === "check/") {
+     parsedParam =  req?.ip || req.headers["x-forwarded-for"]?.split(',').shift() || req?.socket?.remoteAddress || "check/";
+  }
+  console.log("param coming", parsedParam);
   axios
     .get(`http://api.ipstack.com/${parsedParam}`, {
       params: {
         access_key: "96bc7384404d1f6616513ac28fb5f289",
+        headers: {},
       },
     })
     .then(({ data }) => {
